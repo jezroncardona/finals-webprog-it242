a79af42d12e15570748fe53d07653b4cb17bac4b
jezroncardona <jdcardona@student.apc.edu.ph>
2026-02-20 21:52:28 +0800

    Added live updates to changes in database.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 import PostCards from './components/PostCards';
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+)
+
 //the actual app display
 function BulletinBoard() {
   const [posts, setPosts] = useState([]);
@@ -12,16 +17,36 @@ function BulletinBoard() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const baseUrl = process.env.REACT_APP_API_URL.replace(/\/$/, ""); // Removes any trailing slash
-        const response = await axios.get(`${baseUrl}/app`);
-        setPosts (response.data);
-      } catch (error) {
-        console.error("Error fetching posts: ", error);
+        const {data} =  await axios.get(`${baseUrl}/app`);
+        setPosts(data);
+      } catch (err) {
+        console.error(err);
       } finally {
         setLoading(false);
       }
     };
+
     fetchPosts();
+
+  const subscription = supabase
+    .channel('webprog_stuff:board_post')
+    .on(
+      'postgres_changes', 
+      {
+        event: '*',
+        schema: 'webprog_stuff',
+        table: 'board_post'
+      },
+      (payload) => {
+        fetchPosts;
+      }
+    )
+    .subscribe;
+  
+  return () => {
+    supabase.removeChannel(subscription);
+  }
+
   }, []);
 
   if (loading) return <p>Loading the board...</p>;
